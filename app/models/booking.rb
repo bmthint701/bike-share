d5276c525d8998ae9b0c7aba3c5357f3e42cfc51
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-26 18:28:18 +0900

    added model validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :listing
   belongs_to :user
+  validates :start_date, presence: true, less_than: :end_date
+  validates :end_date, presence: true
 end
