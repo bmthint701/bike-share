731923b68fa994249b425150cf594a830f24d71c
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-26 19:57:45 +0900

    removing less than validator

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :listing
   belongs_to :user
-  validates :start_date, presence: true, less_than: :end_date
+  validates :start_date, presence: true
+  # validates :start_date, presence: true, less_than: :end_date
   validates :end_date, presence: true
 end
