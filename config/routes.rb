a495cb5be273d2940b4de74261fa74293a6d779a
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-26 18:01:11 +0900

    fixed shauns fuckup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'bikes#search'
+  root to: 'listings#search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
