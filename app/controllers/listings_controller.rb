e5f35b80c5642f690bf9efc2b8c23ceb9da926c6
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-26 19:16:03 +0900

    added new method, create in progress

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,13 @@
 class ListingsController < ApplicationController
   def search
   end
+
+  def new
+    @listing = Listing.new()
+  end
+
+  def create
+    # a = Listing.new(@listing, renter_id: current_user.id)
+
+  end
 end
