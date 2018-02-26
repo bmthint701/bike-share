236209d11d9ca5e06975fbefc15f497ef44e03d0
Sean Nikravesh <seannik@bluewin.ch>
2018-02-26 19:04:46 +0900

    bootstrap js added and landing page link updated

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,4 +1,9 @@
 class ListingsController < ApplicationController
   def search
+    # @listings = if params[:term]
+    #   Listing.where('address ILIKE ?', "%#{params[:term]}%").all
+    # else
+    #   Listing.all
+    # end
   end
 end
