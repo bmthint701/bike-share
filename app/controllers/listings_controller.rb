6516a975b0c07aa8364656824c366ac74a17950e
Benjamin Thint <benjamin.thint@gmail.com>
2018-02-27 11:05:55 +0900

    updated associations with proper belongs to, added new page, fnished create

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -8,11 +8,22 @@ class ListingsController < ApplicationController
   end
 
   def new
-    @listing = Listing.new()
+    @listing = Listing.new
   end
 
   def create
-    # a = Listing.new(@listing, renter_id: current_user.id)
+    @listing = Listing.new(listing_params)
+    @listing.renter_id = current_user.id
+    if @listing.save!
+      redirect_to listing_path(@listing)
+    else
+      render:new
+    end
+  end
 
+  private
+
+  def listing_params
+    params.require(:listing).permit(:address, :bike_type, :renter_id, :active, :instruction, :hourly_price, :listing_name, :description)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,9 +1,11 @@
 class Listing < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+  belongs_to :renter, class_name: 'User'
   has_many :photos
   validates :bike_type, presence: true
   validates :active, presence: true
   validates :hourly_price, presence: true, numericality: true
   validates :listing_name, presence: true
   validates :description, presence: true
+  validates :address, presence: true
 end
