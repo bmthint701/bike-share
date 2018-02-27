83b5669821ae628de7b74f908fc067e353e452d1
Yusaku Kasahara <mark.kasahara@gmail.com>
2018-02-27 12:25:33 +0900

    added background image and started seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+listing = Listing.create([{
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  phone: Faker::PhoneNumber.phone_number,
+  email: Faker::Internet.email
+  encrypted_password: "123456"
+
+  }])
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone"
+
+# require 'faker'
+
+# puts 'Creating 100 fake restaurants...'
+# 100.times do
+#   restaurant = Restaurant.new(
+#     name:    Faker::Company.name,
+#     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+#     rating:  (0..5).to_a.sample
+#   )
+#   restaurant.save!
+# end
+# puts 'Finished!'
