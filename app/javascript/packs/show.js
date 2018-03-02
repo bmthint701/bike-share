4b8d2d8e01d2ca9e75f1a6263406537f30384b10
Benjamin Thint <benjamin.thint@gmail.com>
2018-03-02 18:00:56 +0900

    added dynamic pricing

--- app/javascript/packs/show.js
+++ app/javascript/packs/show.js
@@ -1,20 +1,28 @@
-// import moment from 'momentjs'
+import moment from 'moment'
 
 console.log("yay1")
 
-const year_start = document.getElementById("booking_start_date_1i")
+const year = document.getElementById("booking_start_date_1i")
 const month_start = document.getElementById("booking_start_date_2i")
 const day_start = document.getElementById("booking_start_date_3i")
 const hour_start = document.getElementById("booking_start_date_4i")
 const month_end = document.getElementById("booking_end_date_2i")
 const day_end = document.getElementById("booking_end_date_3i")
 const hour_end = document.getElementById("booking_end_date_4i")
-const elements = [month_start, day_start, hour_start, month_end, day_end, hour_end]
 
-elements.forEach((element) => {
+const startDateElements = [month_start, day_start, hour_start]
+const endDateElements = [month_end, day_end, hour_end]
+
+let startDate = moment(new Date(year.value.toString(), month_start.value, day_start.value, hour_start.value))
+let endDate = moment(new Date(year.value.toString(), month_end.value, day_end.value, hour_end.value))
+
+startDateElements.forEach((element) => {
   element.addEventListener('change', (event) => {
     // console.log("<%= j DateTime.new(year_start) %>")
-    console.log("yay")
+    console.log("yay1")
+    startDate = moment(new Date(year.value.toString(), month_start.value, day_start.value, hour_start.value))
+
+    calculateDiff();
 
 
     // document.getElementById(ingredientElement).value = '';
@@ -22,3 +30,29 @@ elements.forEach((element) => {
     // hiddenDoses[counter].classList.add('hidden');
   });
 });
+
+endDateElements.forEach((element) => {
+  element.addEventListener('change', (event) => {
+    // console.log("<%= j DateTime.new(year_start) %>")
+    console.log("yay2")
+
+    console.log({year, month_end, day_end, hour_end})
+    console.log(year.value, month_end.value, day_end.value, hour_end.value)
+    console.log(new Date(year.value.toString(), month_end.value, day_end.value, hour_end.value))
+    endDate = moment(new Date(year.value.toString(), month_end.value, day_end.value, hour_end.value))
+    calculateDiff();
+
+    // document.getElementById(ingredientElement).value = '';
+    // document.getElementById(descriptionElement).value = '';
+    // hiddenDoses[counter].classList.add('hidden');
+  });
+});
+
+function calculateDiff() {
+  console.log({startDate, endDate})
+  if (startDate && endDate) {
+    console.log(endDate.diff(startDate, 'hours'))
+    const price = 500 * endDate.diff(startDate, 'hours')
+    document.getElementById('price').innerText = price;
+  }
+}
